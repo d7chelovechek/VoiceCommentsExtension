0c5228c6b8560e5e16787a6442950c549c66971e
Ayrat Khaydarov <info.chelovechek@gmail.com>
2023-09-23 03:42:03 +0300

    Issue #1
    ● Improved LICENSE;
    ● Improved vsixmanifest;
    ● Implemented Recorder window;
    ● Implemented OpenRecorderWindow command for show Recorder window as dialog;
    ● Implamented PasteVoiceComment command for paste text to code editor.

---
+++ VoiceCommentsExtension/Commands/PasteVoiceCommentCommand.cs
@@ -0,0 +1,21 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using System.Threading.Tasks;
+
+namespace VoiceCommentsExtension.Commands
+{
+    [Command(PackageGuids.VoiceCommentsExtensionPackageString, PackageIds.PasteVoiceCommentCommandId)]
+    public class PasteVoiceCommentCommand : BaseCommand<PasteVoiceCommentCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            DocumentView document = await VS.Documents.GetActiveDocumentViewAsync();
+
+            if (document?.TextView.Caret.Position.BufferPosition is SnapshotPoint position)
+            {
+                document?.TextBuffer.Insert(position, "Command is working");
+            }
+        }
+    }
+}
---
+++ VoiceCommentsExtension/VoiceCommentsExtensionPackage.cs
@@ -0,0 +1,25 @@
+﻿using Community.VisualStudio.Toolkit;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VoiceCommentsExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.VoiceCommentsExtensionPackageString)]
+    public sealed class VoiceCommentsExtensionPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(
+            CancellationToken cancellationToken, 
+            IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
